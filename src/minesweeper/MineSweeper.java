1c72c1ea0dfcd770f9bf593bdf5163701599da3b
Rémi Campistron <remi.campistron@gmail.com>
2025-11-05 15:51:43 +0100

    Gestion des cases et des bombes

--- src/minesweeper/MineSweeper.java
+++ src/minesweeper/MineSweeper.java
@@ -4,54 +4,65 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Map.Entry;
 
 public class MineSweeper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Définit la taille de la grille
 		int rows = 6;
 		int cols = 12;
 		
-		printGrid(rows, cols);
+		
 		Scanner scan = new Scanner(System.in);
+		
+		// va stocker les coordonnées des bombes
 		Map <Integer, int []> bombs = new HashMap<>();
 		// les coordonnées des coups joués par le joueur
 		Map <Integer, int []> playerMoves = new HashMap<>();
+		
+		// nombre de bombes
 		int nbBombs = 9;
 		
+		// remplit la hasmap des coordonnées des bombes 
 		for (int i =0; i <nbBombs;i++) {			
 			bombs.put(i,calculateBombPosition(bombs,rows,cols));
 		}
 		
+		//Affiche la grille
+		printGrid(rows, cols,bombs,playerMoves);
 		
-		// Debug print of bomb coordinates
-        for (Entry<Integer, int[]> bomb : bombs.entrySet()) {
-            int[] b = bomb.getValue();
-            System.out.println("Bomb " + bomb.getKey() + " -> (" + b[0] + ", " + b[1] + ")");
-        }
+		// compteur qui enregistre le nombre de coups joués
         int nbPlayerMoves = 0;
         boolean gameOver = false;
         while(!gameOver) {
+        	 
+        	 // Input utilisateur des coordonnées
         	 int inputX = readIntInRange(scan, "Rentrer une coordonnée en x", 0, cols - 1);
         	 int inputY = readIntInRange(scan, "Rentrer une coordonnée en y", 0, rows - 1);
+        	 
+        	 
         	 nbPlayerMoves ++;
-        	 
-        	 
+        	 //enregistre les coordonnées des coups joués
         	 playerMoves.put(nbPlayerMoves,new int[] {inputX,inputY});
+        	 printGrid(rows, cols,bombs,playerMoves);
         	 
+        	 //le coup joué tombe sur une bombe, on affiche la grille, la partie est finie
         	 if(isAMinedTile(inputX,inputY,bombs)) {
-        		 printGrid(rows, cols);
+        		 printGrid(rows, cols,bombs,playerMoves);
         		 System.out.println("Game over :( ");
+        		 gameOver = true;
         	 }
         	 // L'utilisateur quitte le jeu 
-        	 if (inputX < 0 || inputY < 0) break;        	
+        	 if (inputX < 0 || inputY < 0) gameOver = true;        	
         }
 	}
 	
 
-
+	
 	private static boolean isAMinedTile(int inputX, int inputY, Map<Integer, int[]> bombs) {
+		/**
+		 * Determine si la case jouée contient une bombe
+		 */
 		for (int[] bombArr: bombs.values()) {	
 			if(bombArr[0] == inputX && bombArr[1] == inputY) {
 				return true;
@@ -61,8 +72,11 @@ public class MineSweeper {
 	}
 
 
-
+	
 	private static int readIntInRange(Scanner scan, String prompt, int min, int max) {
+		/**
+		 * Valide les inputs de coordonnées par l'utilisateur
+		 */
 	    while (true) {
 	        System.out.print(prompt + " [" + min + "-" + max + "] : ");
 	        String userInputString = scan.nextLine().trim();
@@ -71,6 +85,7 @@ public class MineSweeper {
 	        if (userInputString.equalsIgnoreCase("quit")) return -1; 
 
 	        if (!userInputString.isEmpty()) {
+	        	// contrôle si tous les charactères sont des chiffres
 	            boolean allDigits = true;
 	            for (int i = 0; i < userInputString.length(); i++) {
 	                if (!Character.isDigit(userInputString.charAt(i))) {
@@ -79,6 +94,7 @@ public class MineSweeper {
 	                }
 	            }
 	            if (allDigits) {
+	            	// contrôle que les valeurs données sont bien entre les bornes fournies
 	                int val = Integer.parseInt(userInputString);
 	                if (val >= min && val <= max) return val;
 	            }
@@ -108,8 +124,12 @@ public class MineSweeper {
 		}		
 	}
 
-	public static void printGrid(int rows, int cols) {
-		
+	public static void printGrid(int rows, int cols,Map<Integer, int[]> bombs,Map<Integer, int[]> playerMoves) {
+		/**
+		 * Gère l'affichage de la grille , l'affichage des bombes et l'affichage des cases découvertes
+		 */
+		boolean isABomb;
+		boolean isDiscovered;
 		System.out.print(" ".repeat(3));
 		for (int j =0; j<cols;j++) {
 			 System.out.printf("%3d", j);
@@ -118,13 +138,52 @@ public class MineSweeper {
 		
 		for(int i = 0; i<rows; i++) {
 			System.out.printf("%3d", i);
-			for (int j =0; j<cols;j++) {						
-				System.out.printf("%3s", "+");
+			for (int j =0; j<cols;j++) {
+				isABomb = false;
+				isDiscovered = false;
+				for (int[] bombArr: bombs.values()) {
+					if(bombArr[0] == j && bombArr[1] == i) {
+						isABomb = true;
+					}
+				}
+				for (int[] movesArr: playerMoves.values()) {
+					if(movesArr[0] == j && movesArr[1] == i) {
+						isDiscovered = true;
+					}
+				}
+				int nbBombs = nbBombsAround(i,j,bombs);
+				if(isABomb) {
+					System.out.printf("%3s", "B");
+				} else if(isDiscovered){
+					System.out.printf("%3s", nbBombs);
+				} else {
+					System.out.printf("%3s", "x");
+				}
 			}
 				
 			
 			System.out.println();
 		}
+	}
+
+
+
+	private static int nbBombsAround(int x, int y, Map<Integer, int[]> bombs) {
+		/*
+		 * Calcule les bombes aux alentours de la case découverte, si la valeur absolue de la différence de coordonées x (ou y)
+		 *  est inferieure ou egale a 1 cela veut dire qu'une bombe se trouve aux alentours
+		 */
+		int nbBombs = 0;
+		for (int[] bombArr: bombs.values()) {
+			 int dx = Math.abs(bombArr[0] - x);
+		        int dy = Math.abs(bombArr[1] - y);
+		        // voisin si |dx|<=1 et |dy|<=1, en excluant la case (x,y) elle-même
+		        if ((dx <= 1 && dy <= 1) && !(dx == 0 && dy == 0)) {
+		            nbBombs++;
+		        }
+		 
+		}
+		return nbBombs;
 	}
 
 }
